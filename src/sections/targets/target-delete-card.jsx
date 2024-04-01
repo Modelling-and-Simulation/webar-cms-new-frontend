f2a9cdc1001a07955a2544ad0fec1d6bd43b4af7
Sachintha Gunaratne <sachinthasl99@gmail.com>
2024-04-01 15:50:24 +0530

    fix: refresh targets after edit and delete

--- src/sections/targets/target-delete-card.jsx
+++ src/sections/targets/target-delete-card.jsx
@@ -1,7 +1,6 @@
 // DeleteConfirmation.js
 import React from 'react';
 import PropTypes from 'prop-types';
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Dialog from '@mui/material/Dialog';
@@ -10,46 +9,22 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 
-import { useRouter } from 'src/routes/hooks';
-
-import useApi from 'src/hooks/useApi';
-
-const TargetDeleteCard = ({ target, onClose, onDelete }) => {
-  const router = useRouter();
-  const { deleteTarget } = useApi();
-
-  const handleDelete = () => {
-    deleteTarget(target._id).then((response) => {
-      toast.success('Target deleted successfully');
-      router.reload();
-    }).catch((error) => {
-      console.error('Error deleting target', error);
-      toast.error('Error deleting target');
-    }).finally(() => {
-      // setStatusMsg('');
-    })
-    onDelete();
-    onClose();
-  };
-
-  return (
-    <Dialog open onClose={onClose}>
-      <DialogTitle>Confirm Deletion</DialogTitle>
-      <DialogContent>
-        Are you sure you want to delete this target?
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={handleDelete} color="error">Delete</Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
+const TargetDeleteCard = ({ onClose, onDelete }) => (
+  <Dialog open onClose={onClose}>
+    <DialogTitle>Confirm Deletion</DialogTitle>
+    <DialogContent>
+      Are you sure you want to delete this target?
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={onClose}>Cancel</Button>
+      <Button onClick={onDelete} color="error">Delete</Button>
+    </DialogActions>
+  </Dialog>
+);
 
 TargetDeleteCard.propTypes = {
-    target: PropTypes.func.isRequired,
-    onClose: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default TargetDeleteCard;
--- src/sections/targets/target-edit-card.jsx
+++ src/sections/targets/target-edit-card.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import { toast } from "react-toastify";
 import React, { useState } from 'react';
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,14 +9,8 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 
-import { useRouter } from 'src/routes/hooks';
 
-import useApi from 'src/hooks/useApi';
-
-const TargetEditCard = ({ target, onClose, onSave }) => {
-  const router = useRouter();
-  const { editTarget } = useApi();
-
+const TargetEditCard = ({ target, onClose, onConfirm }) => {
   const [editedContent, setEditedContent] = useState({ ...target });
 
   const handleChange = (e) => {
@@ -29,20 +22,7 @@ const TargetEditCard = ({ target, onClose, onSave }) => {
   };
 
   const handleSave = () => {
-    editTarget(target._id, {
-      targetName: editedContent.targetName,
-      description: editedContent.description,
-    }).then((response) => {
-      toast.success('Target updated successfully');
-      router.reload();
-    }).catch((error) => {
-      console.error('Error updating target', error);
-      toast.error('Error updating target');
-    }).finally(() => {
-      // setStatusMsg('');
-    })
-    onSave(editedContent);
-    onClose();
+    onConfirm(editedContent);
   };
 
   return (
@@ -78,7 +58,7 @@ const TargetEditCard = ({ target, onClose, onSave }) => {
 TargetEditCard.propTypes = {
   target: PropTypes.object.isRequired,
   onClose: PropTypes.func.isRequired,
-  onSave: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
 };
 
 export default TargetEditCard;
